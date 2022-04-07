d840edfb4c7240ca4a9bc80711b4f2ef748f62d9
yoniks <89047399+yoniks@users.noreply.github.com>
2022-04-07 14:54:04 +0300

    Update main.cpp

--- code_c_plusplus/Reference Parameters/main.cpp
+++ code_c_plusplus/Reference Parameters/main.cpp
@@ -13,12 +13,30 @@ User-defined header files: These files are defined by the user and can be import
  #include "vvsr.h"
 
 using namespace std;
-
+void neg(&x);
 
 int main(){
     char str[]="Hello word!";
-    uuper_string_by_ref(str[0]);
+ //Call-by-reference passes the address of the argument to the function
+ //code within the function can change the value of the argument outside the function.
+    uuper_string_by_ref(str[0]);// or str
+// using call-by-value, a copy of the argument is passed to the function.
+ //this means that code within a function cannot alter the arguments used to call the function.
    add_dash_to_space_by_value(str);// pointer first address
     cout<<str<<"\n";
+ 
+ 
+    // if we not send not array in c++, there is another way sending by ref 
+     //for example call by refernce
+     int x; 
+     x = 10;
+     cout << x << " negated is ";
+     neg(x); // no longer need the & operator
+     cout << x << "\n";
     return 0;
 }
+
+   void neg(int &i)
+   {
+     i = -i;     // i is now a reference, don't need *
+   }
